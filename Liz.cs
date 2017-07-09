f1a1f28eb30324ca31502090e059562b657e0444
advence-liz <1012515183@qq.com>
2017-07-09 15:18:27 +0800

    添加基础文件

---
+++ Liz.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace hwapp
+{
+    class Liz 
+    {
+      public string name = "liz";
+       public  string Say (){
+            return this.name;
+       } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   var liz=new Liz();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(liz.Say());
+        }
+    }
+}
